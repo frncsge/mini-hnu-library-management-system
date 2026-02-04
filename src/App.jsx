fca49f2f294e285795c9c5e49f98ba18fdd6514e
Francis Ge <francisge527@gmail.com>
2026-02-04 19:40:43 +0800

    add login route

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,23 @@
+import { createBrowserRouter } from "react-router";
+import { RouterProvider } from "react-router/dom";
+
 function App() {
-  return (
-    <div className="h-screen flex items-center justify-center">
-      <h1>Hello motherfucker!</h1>
-    </div>
-  );
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <div>This is the landing page?</div>,
+    },
+    {
+      path: "/login",
+      element: <div>This is the login page</div>,
+    },
+  ]);
+
+  return <RouterProvider router={router} />;
 }
 
 export default App;
+
+//to do:
+// / route should be a page where users can select to log in as admin, librarian, student
+//or maybe just a regular log in page and then just check the role of the log ins
