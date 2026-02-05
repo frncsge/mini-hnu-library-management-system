07280d9a594c398003ce59e4f8b5b0f02151f3e4
Francis Ge <francisge527@gmail.com>
2026-02-05 21:48:18 +0800

    add auth route

--- server/server.js
+++ server/server.js
@@ -1,7 +1,14 @@
 import express from "express";
+import authRoutes from "./routes/auth.routes.js";
 
 const app = express();
 const PORT = 3000;
+
+//middlewares
+app.use(express.json());
+
+//auth routes
+app.use(authRoutes);
 
 app.listen(PORT, () => {
   console.log("Listening on port", PORT);
