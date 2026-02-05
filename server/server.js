95ba9a54fa9bb04a61af63204e8e59da01d5d41c
Francis Ge <francisge527@gmail.com>
2026-02-05 23:37:08 +0800

    add middlewares

--- server/server.js
+++ server/server.js
@@ -1,11 +1,20 @@
 import express from "express";
 import authRoutes from "./routes/auth.routes.js";
+import cookieParser from "cookie-parser";
+import cors from "cors";
 
 const app = express();
 const PORT = 3000;
 
 //middlewares
 app.use(express.json());
+app.use(cookieParser());
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  }),
+);
 
 //auth routes
 app.use(authRoutes);
