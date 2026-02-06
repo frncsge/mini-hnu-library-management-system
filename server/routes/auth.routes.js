5fa7a02620675ef1b8416ef34baaf08cb2ac93c5
Francis Ge <francisge527@gmail.com>
2026-02-06 16:34:07 +0800

    add /login/verify-token route

--- server/routes/auth.routes.js
+++ server/routes/auth.routes.js
@@ -1,8 +1,10 @@
 import express from "express";
 import login from "../controllers/login.controller.js";
+import verifyOtp from "../controllers/verifyOtp.controller.js";
 
 const router = express.Router();
 
 router.post("/login", login);
+router.post("/login/verify-otp", verifyOtp);
 
 export default router;
