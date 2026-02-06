b153fb44f1779524055397d662dd8f443934f19c
Francis Ge <francisge527@gmail.com>
2026-02-07 02:21:34 +0800

    add route for resending otp

--- server/routes/auth.routes.js
+++ server/routes/auth.routes.js
@@ -1,10 +1,13 @@
 import express from "express";
 import login from "../controllers/login.controller.js";
 import verifyOtp from "../controllers/verifyOtp.controller.js";
+import resendOtp from "../controllers/resendOtp.controller.js";
+import { checkLoggedIn } from "../middlewares/auth.middleware.js";
 
 const router = express.Router();
 
-router.post("/login", login);
+router.post("/login", checkLoggedIn, login);
 router.post("/login/verify-otp", verifyOtp);
+router.post("/login/resend-otp", checkLoggedIn, resendOtp);
 
 export default router;
