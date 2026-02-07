ccfce1edc315b69b0cda21d1456fab636b210975
Francis Ge <francisge527@gmail.com>
2026-02-07 22:30:49 +0800

    add queries for user related

--- server/models/user.model.js
+++ server/models/user.model.js
@@ -11,3 +11,72 @@ export const getUserByEmail = async (email) => {
     throw error;
   }
 };
+
+export const getAdminProfile = async (id) => {
+  try {
+    const result = await pool.query(
+      `
+        SELECT 
+	        u.user_id,
+	        u.email,
+	        ad.admin_id,
+	        ad.admin_username,
+        FROM users u
+        JOIN administrator ad ON u.user_id = ad.user_id
+        WHERE u.user_id = $1; 
+      `,
+      [id],
+    );
+    return result.rows[0];
+  } catch (error) {
+    console.error("Error getting admin profile:", error);
+    throw error;
+  }
+};
+
+export const getLibrarianProfile = async (id) => {
+  try {
+    const result = await pool.query(
+      `
+        SELECT 
+	        u.user_id,
+	        u.email,
+	        lib.librarian_id,
+	        lib.first_name,
+          lib.last_name
+        FROM users u
+        JOIN librarian lib ON u.user_id = lib.user_id
+        WHERE u.user_id = $1; 
+      `,
+      [id],
+    );
+    return result.rows[0];
+  } catch (error) {
+    console.error("Error getting librarian profile:", error);
+    throw error;
+  }
+};
+
+export const getStudentProfile = async (id) => {
+  try {
+    const result = await pool.query(
+      `
+        SELECT 
+	        u.user_id,
+	        u.email,
+	        st.student_id,
+	        st.student_school_id,
+	        st.first_name,
+	        st.last_name
+        FROM users u
+        JOIN student st ON u.user_id = st.user_id
+        WHERE u.user_id = $1; 
+      `,
+      [id],
+    );
+    return result.rows[0];
+  } catch (error) {
+    console.error("Error getting student profile:", error);
+    throw error;
+  }
+};
