20df008d8228404adc8c723f3c3bfe0850d16c8f
Francis Ge <francisge527@gmail.com>
2026-02-09 07:58:48 +0800

    minor changes

--- src/pages/StudentHomePage.jsx
+++ src/pages/StudentHomePage.jsx
@@ -1,68 +1,11 @@
-import { useState, useEffect, useCallback } from "react";
+import { useState, useEffect } from "react";
 import { useNavigate } from "react-router";
 
-function StudentHomePage() {
-  const API_URL = import.meta.env.VITE_API_URL;
+function StudentHomePage({ profile }) {
   const [loading, setLoading] = useState(true);
-  const [message, setMessage] = useState("");
-  const [profile, setProfile] = useState(null);
+  const [error, setError] = useState(null);
   const [menuOpen, setMenuOpen] = useState(false);
   const navigate = useNavigate();
-
-  useEffect(() => {
-    //for refreshing session
-    async function fetchRefresh() {
-      try {
-        const response = await fetch("/api/refresh", {
-          method: "POST",
-          credentials: "include",
-        });
-
-        return response.ok;
-      } catch (error) {
-        return false;
-      }
-    }
-
-    async function fetchProfile(retried = false) {
-      try {
-        const response = await fetch(API_URL + "/api/me", {
-          method: "GET",
-          credentials: "include",
-        });
-        const data = await response.json();
-
-        if (response.ok) {
-          setProfile(data.profile);
-          setLoading(false);
-          return;
-        }
-
-        if (response.status === 401 && !retried) {
-          //try refreshing session once
-          const refreshed = await fetchRefresh();
-          if (refreshed) return fetchProfile(true); //retry once
-
-          setLoading(false);
-          alert("Session expired. Please login in again");
-          navigate("/login", { replace: true });
-          return;
-        }
-
-        setLoading(false);
-        setMessage(data.message);
-        alert(data.message);
-        return;
-      } catch (error) {
-        setLoading(false);
-        setMessage(data.message);
-        console.error("Error fetching user profile", error);
-        alert("Unable to connect to the server");
-      }
-    }
-
-    fetchProfile();
-  }, []);
 
   async function handleLogout() {
     try {
@@ -82,9 +25,6 @@ function StudentHomePage() {
       alert("Unable to connect to the server");
     }
   }
-
-  if (loading) return <div>Loading...</div>;
-  if (!loading && !profile) return <div>{message}</div>;
 
   return (
     <>
