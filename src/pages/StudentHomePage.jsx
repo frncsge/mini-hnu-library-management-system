d60aa7e69102e5ac7a644dc5cbc677f58f89a17c
Francis Ge <francisge527@gmail.com>
2026-02-08 11:38:32 +0800

    fetch api for user profile resource and redirect user to login if unauthenticated

--- src/pages/StudentHomePage.jsx
+++ src/pages/StudentHomePage.jsx
@@ -1,5 +1,97 @@
+import { useState, useEffect } from "react";
+import { useNavigate } from "react-router";
+
 function StudentHomePage() {
-  return <div>Student Home Page</div>;
+  const API_URL = import.meta.env.VITE_API_URL;
+  const [loading, setLoading] = useState(true);
+  const [profile, setProfile] = useState(null);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    //for refreshing session
+    async function fetchRefresh() {
+      try {
+        const response = await fetch("/api/refresh", {
+          method: "POST",
+          credentials: "include",
+        });
+
+        return response.ok;
+      } catch (error) {
+        return false;
+      }
+    }
+
+    async function fetchProfile(retried = false) {
+      try {
+        const response = await fetch(API_URL + "/api/me", {
+          method: "GET",
+          credentials: "include",
+        });
+        const data = await response.json();
+
+        if (response.ok) {
+          setProfile(data.profile);
+          setLoading(false);
+          return;
+        }
+
+        if (response.status === 401 && !retried) {
+          //try refreshing session once
+          const refreshed = await fetchRefresh();
+          if (refreshed) return fetchProfile(true); //retry once
+
+          setLoading(false);
+          alert("Session expired. Please login in again");
+          navigate("/login", { replace: true });
+          return;
+        }
+
+        setLoading(false);
+        alert(data.message);
+        return;
+      } catch (error) {
+        setLoading(false);
+        console.error("Error fetching user profile", error);
+        alert("Unable to connect to the server");
+      }
+    }
+
+    fetchProfile();
+  }, []);
+
+  if (loading || !profile) return <div>Loading Student Home Page...</div>;
+
+  return (
+    <>
+      <nav className="flex items-center justify-between bg-green-500 px-4 py-5">
+        <section className="font-bold text-lg text-white cursor-pointer">
+          Mini HNU Library MS
+        </section>
+        <section>
+          <ul className="flex gap-5 text-white">
+            <li className="cursor-pointer">Home</li>
+            <li className="cursor-pointer">Books</li>
+            <li className="cursor-pointer">Borrowed</li>
+          </ul>
+        </section>
+        <section className="text-white cursor-pointer">
+          {`${profile.first_name} ${profile.last_name}`}
+        </section>
+      </nav>
+      <main className="px-4 pt-6">
+        <section className="flex items-start gap-3">
+          <div className="w-[100px] h-[100px] bg-gray-200 flex items-center justify-center">
+            <span className="text-white text-2xl font-bold">?</span>
+          </div>
+          <div>
+            <h2 className="font-bold text-lg">{`${profile.first_name} ${profile.last_name} [${profile.student_school_id}]`}</h2>
+            <p className="text-sm">{profile.email}</p>
+          </div>
+        </section>
+      </main>
+    </>
+  );
 }
 
 export default StudentHomePage;
