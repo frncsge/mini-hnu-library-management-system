dded2dcc98f744dd60d51ecad3594b3cb268b593
Francis Ge <francisge527@gmail.com>
2026-02-09 07:56:34 +0800

    add route for getting dashboard summary

--- server/routes/book.routes.js
+++ server/routes/book.routes.js
@@ -1,15 +1,25 @@
 import express from "express";
-import { authenticateUser } from "../middlewares/auth.middleware.js";
+import {
+  authenticateRole,
+  authenticateUser,
+} from "../middlewares/auth.middleware.js";
 import {
   getBooks,
   getBook,
   searchBook,
+  getBookDashboardSummary,
 } from "../controllers/book.controller.js";
 
 const router = express.Router();
 
 router.get("/api/books", authenticateUser, getBooks);
-router.get("/api/books/:id", authenticateUser, getBook);
 router.get("/api/books/search", authenticateUser, searchBook);
+router.get(
+  "/api/books/summary",
+  authenticateUser,
+  authenticateRole("admin", "librarian"),
+  getBookDashboardSummary,
+);
+router.get("/api/books/:id", authenticateUser, getBook);
 
 export default router;
