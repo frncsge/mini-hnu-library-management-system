10f1c0e8b2eb21c3f60be339041e295d4aba3157
Francis Ge <francisge527@gmail.com>
2026-02-08 13:55:23 +0800

    add route for logout

--- server/routes/auth.routes.js
+++ server/routes/auth.routes.js
@@ -4,12 +4,14 @@ import verifyOtp from "../controllers/verifyOtp.controller.js";
 import resendOtp from "../controllers/resendOtp.controller.js";
 import { checkLoggedIn } from "../middlewares/auth.middleware.js";
 import getNewAccessToken from "../controllers/refresh.controller.js";
+import logout from "../controllers/logout.controller.js";
 
 const router = express.Router();
 
-router.post("/api/login", checkLoggedIn, login);
+router.post("/api/login", login);
 router.post("/api/login/verify-otp", verifyOtp);
-router.post("/api/login/resend-otp", checkLoggedIn, resendOtp);
+router.post("/api/login/resend-otp", resendOtp);
 router.post("/api/refresh", getNewAccessToken);
+router.post("/api/logout", logout);
 
 export default router;
