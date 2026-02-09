25fb9b0dbe6c4c0dce1016fbe26c75965e3ebf54
Francis Ge <francisge527@gmail.com>
2026-02-09 12:18:36 +0800

    add queries for storing new student account and getting student by school id

--- server/models/user.model.js
+++ server/models/user.model.js
@@ -12,6 +12,55 @@ export const getUserByEmail = async (email) => {
   }
 };
 
+export const getStudentBySchoolId = async (studentSchoolId) => {
+  try {
+    const result = await pool.query(
+      "SELECT * FROM student WHERE student_school_id = $1",
+      [studentSchoolId],
+    );
+    return result.rows[0];
+  } catch (error) {
+    console.error("Error getting student by school id:", error);
+    throw error;
+  }
+};
+
+export const storeNewStudent = async ({
+  email,
+  hashedPassword,
+  firstName,
+  lastName,
+  studentSchoolId,
+}) => {
+  const role = "student";
+  const client = await pool.connect();
+
+  try {
+    await client.query("BEGIN");
+
+    const result = await client.query(
+      "INSERT INTO users (email, hashed_password, user_role) VALUES ($1, $2, $3) RETURNING user_id",
+      [email, hashedPassword, role],
+    );
+    const newUser = { sub: result.rows[0].user_id, role };
+
+    await client.query(
+      "INSERT INTO student (user_id, student_school_id, first_name, last_name) VALUES ($1, $2, $3, $4)",
+      [newUser.sub, studentSchoolId, firstName, lastName],
+    );
+
+    await client.query("COMMIT");
+
+    return newUser;
+  } catch (error) {
+    console.error("Error storing new student:", error);
+    await client.query("ROLLBACK");
+    throw error;
+  } finally {
+    client.release();
+  }
+};
+
 export const getAdminProfile = async (id) => {
   try {
     const result = await pool.query(
