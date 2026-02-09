e2b5f2c222dad7a49bf0f8b5c15d9f043bd192d4
Francis Ge <francisge527@gmail.com>
2026-02-09 12:18:51 +0800

    add register route

--- server/routes/auth.routes.js
+++ server/routes/auth.routes.js
@@ -5,12 +5,14 @@ import resendOtp from "../controllers/resendOtp.controller.js";
 import { checkLoggedIn } from "../middlewares/auth.middleware.js";
 import getNewAccessToken from "../controllers/refresh.controller.js";
 import logout from "../controllers/logout.controller.js";
+import register from "../controllers/registerStudent.controller.js";
 
 const router = express.Router();
 
 router.post("/api/login", login);
 router.post("/api/login/verify-otp", verifyOtp);
 router.post("/api/login/resend-otp", resendOtp);
+router.post("/api/register", register);
 router.post("/api/refresh", getNewAccessToken);
 router.post("/api/logout", logout);
 
